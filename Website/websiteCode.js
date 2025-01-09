fb4ddbaad678ee3541ffc9ac9c234acd8473c12a
J <159636034+Raddevv@users.noreply.github.com>
2025-01-09 13:50:52 +0100

    Add files via upload

---
+++ Website/websiteCode.js
@@ -0,0 +1,18 @@
+const goalDescriptions = {
+    1: "Becoming a full-stack developer means mastering front-end and back-end technologies.",
+    2: "Accumulating experience allows you to solve more complex problems effectively.",
+    3: "Constructing a high-end platform involves creating robust, scalable, and efficient systems.",
+    4: "Founding your own company gives you the opportunity to innovate and lead in your field."
+};
+
+const dropdown = document.getElementById("goalsDropdown");
+const dynamicText = document.getElementById("dynamicText");
+
+
+function changeText() {
+    const selectedValue = dropdown.value;
+    dynamicText.textContent = goalDescriptions[selectedValue];
+}
+
+
+dropdown.addEventListener("change", changeText);
